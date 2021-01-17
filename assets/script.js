33739dc7be032e4db92b5729cbb61f477d1d665a
b00kbaker <caimmurphy@gmail.com>
2021-01-17 13:18:05 -0700

    Password lengthdefined, began 2nd function to join a variety of characters

--- assets/script.js
+++ assets/script.js
@@ -19,7 +19,7 @@
 
 // Assignment Code
 
-var enter;
+var passwordLength;
 var numbers;
 var upperCase;
 var lowerCase;
@@ -27,10 +27,15 @@ var specialCharacters;
 var options;
 var generateBtn = document.querySelector("#generate");
 
-num= ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-upper= ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-lower= ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-character= [' ', '!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', '"', "'"];
+// num= ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'].split("");
+// upper= ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'].split("");
+// lower= ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'].split("");
+// character= [' ', '!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', '"', "'"].split("");
+
+num="0123456789".split("");
+upper="ABCDEFGHIJKLMNOPQRSTYVWXYZ".split("");
+lower="abcdefghijklmnopqrstuvwxyz".split("");
+character="!?#@$%&*".split("");
 
 
 
@@ -42,11 +47,13 @@ window.addEventListener("Click", function() {
 
 
 function generatePassword() {
-  enter = parseInt(prompt("How long would you like the password? Please pick a number between 8 - 128."));
+  passwordLength = parseInt(prompt("How long would you like the password? Please pick a number between 8 - 128."));
+  console.log(generatePassword);
 
-} if (enter < 8 || enter > 128) {
+} if (passwordLength < 8 || passwordLength > 128) {
     alert("The number you selected is outside the paramaters of 8-128, please adjust.");
-  
+  return generatePassword();
+
   
 } else {
    numbers= confirm("Include numbers?");
@@ -57,17 +64,15 @@ function generatePassword() {
 
 if(!numbers && !lowerCase && !upperCase && !specialCharacters) {
  options= alert("At least one option needs to be selected!");
+ return generatePassword();
 }
 
-  
 
-
-// Write password to the #password input
    function writePassword(){
         var password = generatePassword();
         var passwordText = document.querySelector("#password");
-
-        passwordText.value = password;
+        var passwordArray= [];
+        // passwordText.value = password;
 
     }
 
@@ -76,3 +81,6 @@ generateBtn.addEventListener("click", writePassword);
 
 
 generatePassword();
+
+
+
